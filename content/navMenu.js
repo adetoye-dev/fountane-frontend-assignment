6e9af2a98e14c95e47f9c4acb1ec5e9c321d9de9
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-07-31 02:01:46 +0100

    Create side bar menu

---
+++ content/navMenu.js
@@ -0,0 +1,18 @@
+export const routes = [
+  {
+    slug: "/movies/now-playing",
+    label: "Now Playing",
+  },
+  {
+    slug: "/movies/popular",
+    label: "Popular",
+  },
+  {
+    slug: "/movies/upcoming",
+    label: "Upcoming",
+  },
+  {
+    slug: "/movies/top-rated",
+    label: "Top Rated",
+  },
+];
