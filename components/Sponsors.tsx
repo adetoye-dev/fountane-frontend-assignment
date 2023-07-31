649b44315b91fcfb4bcda3499a97a1f324784c33
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-08-01 00:32:37 +0100

    create sponsors component

---
+++ components/Sponsors.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+
+const Sponsors = () => {
+  return (
+    <div className="opacity-60 mt-auto justify-start items-start gap-6 inline-flex overflow-x-scroll container-snap">
+      <Image
+        alt="sponsor"
+        width={32}
+        height={12}
+        className="Image17 w-32 h-12"
+        src="/sponsor-icons/image17.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={24}
+        height={12}
+        className="Image18 w-24 h-12"
+        src="/sponsor-icons/image18.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={24}
+        height={12}
+        className="Image19 w-24 h-12"
+        src="/sponsor-icons/image19.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={24}
+        height={12}
+        className="Image20 w-24 h-12"
+        src="/sponsor-icons/image20.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={36}
+        height={12}
+        className="Image21 w-36 h-12"
+        src="/sponsor-icons/image21.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={24}
+        height={12}
+        className="Image22 w-24 h-12"
+        src="/sponsor-icons/image22.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={11}
+        height={12}
+        className="Image23 w-11 h-12"
+        src="/sponsor-icons/image23.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={20}
+        height={12}
+        className="Image24 w-20 h-12"
+        src="/sponsor-icons/image24.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={28}
+        height={12}
+        className="Image25 w-28 h-12"
+        src="/sponsor-icons/image25.svg"
+      />
+      <Image
+        alt="sponsor"
+        width={20}
+        height={12}
+        className="Image26 w-20 h-12"
+        src="/sponsor-icons/image26.svg"
+      />
+    </div>
+  );
+};
+
+export default Sponsors;
