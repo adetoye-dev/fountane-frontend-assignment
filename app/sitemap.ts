bc2977191b5e1e4b035dc0605568af9285fb4ce7
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-08-01 01:35:32 +0100

    setup seo credentials

---
+++ app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "http://localhost:3000",
+      lastModified: new Date(),
+    },
+    {
+      url: "http://localhost:3000/movies",
+      lastModified: new Date(),
+    },
+  ];
+}
---
+++ next-seo-config.ts
@@ -0,0 +1,10 @@
+export const NEXT_SEO_DEFAULTS = {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "http://localhost:3000",
+    title: "Assignment",
+    description: "Frontend developer Assignment",
+    siteName: "Assignment",
+  },
+};
